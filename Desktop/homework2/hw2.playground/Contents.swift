fa5871d4d6ba6dcba3735b68742929666842664d
spezza <spezza@Pros-MacBook-Pro.local>
2020-12-18 00:25:48 +0200

    finished task v.2.0

--- Desktop/homework2/hw2.playground/Contents.swift
+++ Desktop/homework2/hw2.playground/Contents.swift
@@ -49,52 +49,55 @@ class SpezzaBetSystem {
     //Creating a func for login (checking compliance of login and password)
     
     func login(login: String, password: String) throws {
-        guard let user = allUsers[login] else { throw SysErrors.wrongLogin }
+        guard var user = allUsers[login] else { throw SysErrors.wrongLogin }
         guard user.password == password else { throw SysErrors.wrongPass }
         guard !user.isBanned else { throw SysErrors.accessError }
-        allUsers[login]?.isOnline = true
+        user.isOnline = true
+        allUsers[login] = user
     }
     
     //Function logout change the user's status (online or offline). If offline - user is logged out
     
     func logout(login: String) throws {
-        guard let _ = allUsers[login] else { throw SysErrors.wrongLogin}
-        allUsers[login]?.isOnline = false
+        guard var user  = allUsers[login] else { throw SysErrors.wrongLogin}
+        user.isOnline = false
+        allUsers[login] = user
     }
     
     //Here is a function that allow to make a bet if a user is logged in
     
     func makeBet(login: String, bet: String) throws {
-        guard allUsers[login] != nil else { throw SysErrors.wrongLogin}
-        guard allUsers[login]!.isOnline else { throw SysErrors.autorizationFailed }
-        allUsers[login]?.bets.append(bet)
+        guard var user = allUsers[login] else { throw SysErrors.wrongLogin}
+        guard user.isOnline == true else { throw SysErrors.autorizationFailed }
+        user.bets.append(bet)
+        allUsers[login] = user
     }
     
     //Here is a function that returns the list of user's bets from Dictionary
     
     func listOfBets(login: String) throws -> [String] {
-        guard allUsers[login] != nil else { throw SysErrors.wrongLogin}
-        guard allUsers[login]!.isOnline else { throw SysErrors.autorizationFailed }
-        return allUsers[login]!.bets
+        guard let user = allUsers[login] else { throw SysErrors.wrongLogin}
+        guard user.isOnline == true else { throw SysErrors.autorizationFailed }
+        return user.bets
     }
     
     //Here is a function that returns the list of regular users (only for admins)
     
     func listOfUsers(login: String) throws -> Dictionary<String, User> {
-        guard allUsers[login] != nil else { throw SysErrors.wrongLogin}
-        guard allUsers[login]?.status == .admin else { throw SysErrors.accessError }
+        guard let user = allUsers[login] else { throw SysErrors.wrongLogin}
+        guard user.status == .admin else { throw SysErrors.accessError }
         return allUsers.filter {_, user in user.status == .regularUser }
     }
     
     //This function can ban regular users (only for admins)
     
     func banForUser(loginAdmin: String, loginToBan: String) throws {
-        guard allUsers[loginAdmin] != nil else { throw SysErrors.wrongLogin}
-        guard allUsers[loginToBan] != nil else { throw SysErrors.wrongLogin}
-        guard allUsers[loginAdmin]?.status == .admin else { throw SysErrors.accessError}
-        guard allUsers[loginToBan]?.status == .regularUser else { throw SysErrors.accessError}
-        guard allUsers[loginAdmin]!.isOnline else { throw SysErrors.autorizationFailed}
-        allUsers[loginToBan]!.isBanned = true
+        guard let userAdmin = allUsers[loginAdmin] else { throw SysErrors.wrongLogin}
+        guard var userReg = allUsers[loginToBan] else { throw SysErrors.wrongLogin}
+        guard userAdmin.status == .admin else { throw SysErrors.accessError}
+        guard userReg.status == .regularUser else { throw SysErrors.accessError}
+        guard userAdmin.isOnline == true else { throw SysErrors.autorizationFailed}
+        userReg.isBanned = true
         print("Authorization failed. You've banned")
     }
 }
